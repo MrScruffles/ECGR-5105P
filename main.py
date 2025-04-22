47f1022318f08ef14ec65bc49a2fafe0116722da
Ash Zahabiuon <ash@mountaininteractive.com>
2025-04-22 15:34:30 -0400

    d

---
+++ main.py
@@ -0,0 +1,4 @@
+# there is 3 folders
+# ./dataset, ./finalSorted, ./myImages
+
+# now take all the images in side of the dataset and use it to train a model to help me make it so my ./myImages images when ran also get sorted itno the finalSorted after its trained.
